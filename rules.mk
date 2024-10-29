8de56ad08d6fc7a8490268da5db63d97493d85ef
LS <lev.soltys@qaware.de>
2024-10-29 20:43:23 +0100

    created new branch

---
+++ rules.mk
@@ -0,0 +1,13 @@
+OLED_ENABLE = yes
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+# WS2812_DRIVER = vendor # Use the RP2040's PqIO interface
+# RGB_MATRIX_DRIVER = WS2812 # not sure if this is needed
+KEY_OVERRIDE_ENABLE = yes
+CONVERT_TO=liatris
+SPLIT_KEYBOARD = yes
+
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = yes
+WS2812_DRIVER = vendor
+RGB_MATRIX_DRIVER = ws2812
+RGB_MATRIX_CUSTOM_USER = yes
