4556bd447c122ab576b4450350f5154c65472ce0
KaiFireborn <blitzmasterev3@gmail.com>
2024-11-06 13:35:38 +0100

    refactor: changed folder structure

---
+++ formatter-py/layerPreviewGenerator.py
@@ -0,0 +1,236 @@
+import json
+import pyperclip
+from keyboardFormatter import get_file_from_parent_dir
+
+
+
+def read_data():
+    with open(get_file_from_parent_dir("layers_data.json"), "r") as infile:
+        return json.loads(infile.read())
+
+SYMBOL_LOOKUP = {
+    "QK_LOCK": "l",
+    "LCTL": "c",
+    "LSFT": "s",
+    "LALT": "a",
+    "LGUI": "g",
+    "RCTL": "c",
+    "RSFT": "s",
+    "RALT": "a",
+    "RGUI": "g",
+    "NO": " ",
+    "TRNS": "_",
+    "ENT": "e",
+    "BSPC": "b",
+    "SPC": " ",
+    "ESC": "e",
+    "TAB": "t",
+    "CAPS": "c",
+    "COMM": ",",
+    "DOT": ".",
+    "SLSH": "/",
+    "QUOT": "'",
+    "MUTE": "m",
+    "VOLD": "v",
+    "VOLU": "v",
+    "EXCLM": "!",
+    "AT": "@",
+    "HASH": "#",
+    "DLR": "$",
+    "PERC": "%",
+    "CIRC": "^",
+    "ASTR": "*",
+    "AMPR": "&",
+    "LPRN": "(",
+    "RPRN": ")",
+    "UNDS": "_",
+    "PLUS": "+",
+    "LCBR": "{",
+    "RCBR": "}",
+    "PIPE": "|",
+    "COLN": ":",
+    "DQUO": '"',
+    "TILD": "~",
+    "GRV": "`",
+    "EQL": "=",
+    "BSLS": "\\\\",
+    "QUES": "?",
+    "MINS": "-",
+    "LBRC": "[",
+    "RBRC": "]",
+    "GT": ">",
+    "LT": "<",
+    "PAST": "*",
+    "PSLS": "/",
+    "PMNS": "-",
+    "PDOT": ".",
+    "PPLS": "+",
+    "F1": "f",
+    "F2": "f",
+    "F3": "f",
+    "F4": "f",
+    "F5": "f",
+    "F6": "f",
+    "F7": "f",
+    "F8": "f",
+    "F9": "f",
+    "F10": "f",
+    "F11": "f",
+    "F12": "f",
+    "F13": "f",
+    "F14": "f",
+    "F15": "f",
+    "F16": "f",
+    "F17": "f",
+    "F18": "f",
+    "F19": "f",
+    "F20": "f",
+    "F21": "f",
+    "F22": "f",
+    "F23": "f",
+    "F24": "f",
+    "EUR": "e",
+    "DEG": "d",
+    "DELT": "d",
+    "INF": "i",
+    "AUML": "a",
+    "MATH_AND": "a",
+    "MATH_OR": "o",
+    "SUML": "s",
+    "YES": "y",
+    "NO": "n",
+    "ALMEQ": "a",
+    "ALMNE": "n",
+    "PLSMNS": "p",
+    "SECT": "s",
+    "ERR": "e",
+    "MDOT": "m",
+    "MU": "u",
+    "UUML": "u",
+    "NOT": "n",
+    "GTOET": "g",
+    "LTOET": "l",
+    "LEFTA": "l",
+    "RIGHTA": "r",
+    "UPA": "u",
+    "DOWNA": "d",
+    "OUML": "o",
+    "EQUIV": "e",
+    "HOME": "h",
+    "PGUP": "p",
+    "PGDN": "p",
+    "INS": "i",
+    "END": "e",
+    "UP": "u",
+    "DOWN": "d",
+    "LEFT": "l",
+    "RIGHT": "r",
+    "CW_TOGG": "c",
+    "SLEP": "s",
+    "EJCT": "e",
+    "FIND": "f",
+    "SCRSV": "s",
+    "S(KC_PSCR)": "s",
+    "KC_PSCR": "s",
+    "LSA(KC_PSCR)": "s",
+    "LGUI(D)": "d",
+}
+
+def translate_symbol(symbol):
+    symbol = symbol.replace("KC_", "").replace("CKC_", "")
+    symbol = SYMBOL_LOOKUP.get(symbol, symbol[0])
+    
+    return symbol
+
+def translate_all_symbols_in_layer(layer):
+    for i, row in enumerate(layer):
+        for j, column in enumerate(row):
+            layer[i][j] = translate_symbol(layer[i][j])
+    return layer
+            
+            
+def get_row_halves(row):
+    half_index = len(row) // 2
+    left_half = row[:half_index]
+    right_half = row[half_index:]
+    return left_half, right_half
+
+
+def get_preview_halves(layer):
+    layer_preview = {
+        "l": [],
+        "r": []
+    }
+    for row in layer:
+        left_half, right_half = get_row_halves(row)
+        layer_preview["l"].append(left_half)
+        layer_preview["r"].append(right_half)
+    return layer_preview
+
+def render_top_rows(row, side=""):
+    r = " ".join(row)
+    if side == "l":
+        return r + " "*4
+    else:
+        return " "*4 + r
+
+def render_middle_row(row, side=""):
+    r = " ".join(row)
+    return r
+
+def render_bottom_row(row, side=""):
+    r = " ".join(row)
+    if side == "l":
+        return " "*6 + r
+    elif side == "r":
+        return r + " "*6
+
+def render_half(half, side):
+    rendered_half_rows = []
+    for i, row in enumerate(half):
+        if i <= 1:
+            rendered_half_rows.append(render_top_rows(row, side=side))
+        elif i == 2:
+            rendered_half_rows.append(render_middle_row(row, side=side))
+        elif i == 3:
+            rendered_half_rows.append(render_bottom_row(row, side=side))
+        else:
+            raise Exception("Too many rows", half)
+    return "\\n".join(rendered_half_rows)
+            
+    
+
+def generate_preview_per_half(layer):
+    layer = translate_all_symbols_in_layer(layer)
+    layer_preview_halves = get_preview_halves(layer)
+    
+    rendered_left_half = render_half(layer_preview_halves["l"], "l")
+    rendered_right_half = render_half(layer_preview_halves["r"], "r")
+    
+    
+    return (rendered_left_half, rendered_right_half)
+
+def convert_to_code(preview, layer_name):
+    r = ""
+    half_names = ["l", "r"]
+    for i, preview_half in enumerate(preview):
+        r += f"char {layer_name.casefold()}_preview_{half_names[i]}[] = \"{preview_half}\";\n"
+    return r
+
+def output_preview(preview):
+    pyperclip.copy(preview)
+    print("Completed and copied to clipboard.")
+        
+def generate_previews(data):
+    preview_code_total = ""
+    for layer_name in data:
+        preview = generate_preview_per_half(data[layer_name])
+        print("Rendering", layer_name)
+        preview_code_total += convert_to_code(preview, layer_name)
+    
+    return preview_code_total
+
+if __name__ == "__main__":
+    data = read_data()
+    preview = generate_previews(data)
+    output_preview(preview)
