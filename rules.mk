9492c05e5650ec8953d1bc090842fbeadd3fad26
LS <lev.soltys@qaware.de>
2024-11-10 12:08:24 +0100

    chore: clean up config options

--- rules.mk
+++ rules.mk
@@ -1,7 +1,5 @@
 OLED_ENABLE = yes
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-# WS2812_DRIVER = vendor # Use the RP2040's PqIO interface
-# RGB_MATRIX_DRIVER = WS2812 # not sure if this is needed
+ENCODER_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
 CONVERT_TO=liatris
 SPLIT_KEYBOARD = yes
